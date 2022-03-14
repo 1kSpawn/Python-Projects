b43d26089508f6072e1988e2c96825eb50a3fcda
1kSPWN <98278924+1kSpawn@users.noreply.github.com>
2022-03-14 18:51:40 -0500

    Add files via upload

---
+++ Python Class Creation (Tree Class).py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+class Tree():
+    def __init__(self,Tree,Height,Color):
+        self.my_attributes = 'Tree'
+        self.Height = 'Empire State Building'
+        
+        # Attributes
+        # We take in the argument
+        
+        # EXPECT A BOOLEAN TRUE/FALSE
+        self.color = 'True'
+
+
+# In[3]:
+
+
+my_Tree = Tree(Tree = 'Bonzae', Height = 'Empire State Building', Color = 'True')
+
+
+# In[4]:
+
+
+type(my_Tree)
+
+
+# In[5]:
+
+
+my_Tree.Height
+
+
+# In[6]:
+
+
+my_Tree.color
+
+
+# In[7]:
+
+
+my_Tree.my_attributes
+
+
+# In[ ]:
+
+
+
+
